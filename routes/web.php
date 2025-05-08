9f7dfdef54785f60493ad9cc45d146c8deb7f220
shianglin17 <leishianglin@gmail.com>
2025-05-08 23:49:04 +0800

    fix: 所有路由都給 vue-router 處理

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// 將所有前端路由交給Vue處理
+Route::get('/{any}', function () {
     return view('app');
-});
+})->where('any', '.*');
