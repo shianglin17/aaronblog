fc37bcd8dffbf914d14113a8d49552e237e1e204
shianglin17 <leishianglin@gmail.com>
2025-08-15 01:57:33 +0800

    refactor(frontend): 更新標籤API註解說明
    - 明確標示管理API需要認證
    - 區分公開查詢與管理操作的API使用

--- resources/js/api/tagApi.ts
+++ resources/js/api/tagApi.ts
@@ -9,27 +9,27 @@ import { Tag, CreateTagParams, UpdateTagParams } from '../types/tag';
  */
 export const tagApi = {
   /**
-   * 獲取所有標籤列表
+   * 獲取所有標籤列表（公開API，前後台共用）
    */
   getList: (() => http.get(API_ROUTES.TAG.LIST)
     .then(r => r.data)) as ApiFunction<Tag[]>,
 
   /**
-   * 根據 ID 獲取標籤詳情
+   * 根據 ID 獲取標籤詳情（公開API）
    * @param id 標籤 ID
    */
   getById: ((id: number) => http.get(API_ROUTES.TAG.DETAIL(id))
     .then(r => r.data)) as ApiFunction<Tag, number>,
 
   /**
-   * 創建新標籤
+   * 創建新標籤（管理API，需要認證）
    * @param data 標籤資料
    */
   create: ((data: CreateTagParams) => http.post(API_ROUTES.ADMIN.TAG.CREATE, data)
     .then(r => r.data)) as ApiFunction<Tag, CreateTagParams>,
 
   /**
-   * 更新標籤
+   * 更新標籤（管理API，需要認證）
    * @param params 包含 ID 和更新數據的參數
    */
   update: ((params: UpdateTagParams) => 
@@ -37,7 +37,7 @@ export const tagApi = {
     .then(r => r.data)) as ApiFunction<Tag, UpdateTagParams>,
 
   /**
-   * 刪除標籤
+   * 刪除標籤（管理API，需要認證）
    * @param id 標籤 ID
    */
   delete: ((id: number) => http.delete(API_ROUTES.ADMIN.TAG.DELETE(id))
